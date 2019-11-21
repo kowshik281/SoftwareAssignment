29e20a586f5615ebcbcffb59e227693c81d8b9dc
unknown <chaitanyachaitu392@gmail.com>
2019-11-21 22:32:03 +0530

    Done donaaa done

--- menuFile.py
+++ menuFile.py
@@ -1,2 +1,17 @@
 #Menu file
-print("This program is for add,sub,mult and divide")
+import addition,subtraction,multiplication,division
+
+
+
+print("This program is for add,sub,mult and divide\n1.Addition\n2.Subtraction\n3.Multiplication\n4.Division")
+s=int(input("Enter your choice:"))
+if(s==1):
+	print(addition.add())
+elif(s==2):
+	print(subtraction.sub())
+elif(s==3):
+	print(multiplication.mult())
+elif(s==4):
+	print(division.div())
+else:
+	print("Invalid input")	
