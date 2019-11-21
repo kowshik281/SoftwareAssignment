876962f34a5c16daef9d2eefbd7904c131a1c548
unknown <krishnaakash1698@gmail.com>
2019-11-21 22:57:22 +0530

    added modulus

--- menuFile.py
+++ menuFile.py
@@ -1,9 +1,9 @@
 #Menu file
-import addition,subtract,multiplication,division
+import addition,subtract,multiplication,division,modulus
 
 
 
-print("This program is for add,sub,mult and divide\n1.Addition\n2.Subtraction\n3.Multiplication\n4.Division")
+print("This program is for add,sub,mult and divide\n1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.Modulus")
 s=int(input("Enter your choice:"))
 if(s==1):
 	print(addition.add())
@@ -13,5 +13,7 @@ elif(s==3):
 	print(multiplication.mult())
 elif(s==4):
 	print(division.div())
+elif(s==5):
+	print(modulus.mod())
 else:
 	print("Invalid input")	
